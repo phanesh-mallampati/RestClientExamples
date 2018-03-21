6ba86f5f66cc8b44903d2734593cfced43c58b73
phanesh-mallampati <35557714+phanesh-mallampati@users.noreply.github.com>
2018-03-21 12:04:35 +1100

    Initial commit

---
+++ src/main/java/com/phani/restclient/DemoRestClientApplication.java
@@ -0,0 +1,12 @@
+package com.phani.restclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestClientApplication.class, args);
+	}
+}
---
+++ src/test/java/com/phani/restclient/demorestclient/DemoRestClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.phani.restclient.demorestclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoRestClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
